7e07f49d1fc21a3fe5760d5369cf632d95726f63
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2024-05-19 15:40:41 -0400

    Initial Commit

---
+++ ScreenShotApp/ScreenShotAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenShotAppApp.swift
+//  ScreenShotApp
+//
+//  Created by Gabriel Marquez on 2024-05-19.
+//
+
+import SwiftUI
+
+@main
+struct ScreenShotAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
